a356b8e83046a72edbfa995cb82b52b2a7af8bd9
Candice Bentéjac <candice.bentejac@gmail.com>
2025-08-21 17:57:39 +0200

    [core] desc.node: Use node's Python executable to call `meshroom_compute`
    If `pythonExecutable` has not been explicitly set in the node's
    description, then the default `python` will be used. Otherwise, the
    one set in the description will be used.
    
    `_MESHROOM_COMPUTE_EXE` is removed since it is now impossible to
    determine it outside of the node.

--- meshroom/core/desc/node.py
+++ meshroom/core/desc/node.py
@@ -16,7 +16,6 @@ from meshroom.core import cgroup
 
 _MESHROOM_ROOT = Path(meshroom.__file__).parent.parent.as_posix()
 _MESHROOM_COMPUTE = (Path(_MESHROOM_ROOT) / "bin" / "meshroom_compute").as_posix()
-_MESHROOM_COMPUTE_EXE = f"python {_MESHROOM_COMPUTE}"
 
 
 class MrNodeType(enum.Enum):
@@ -257,7 +256,8 @@ class Node(BaseNode):
         return MrNodeType.NODE
 
     def processChunkInEnvironment(self, chunk):
-        meshroomComputeCmd = f"{_MESHROOM_COMPUTE_EXE} \"{chunk.node.graph.filepath}\" --node {chunk.node.name} --extern --inCurrentEnv"
+        meshroomComputeCmd = f"{chunk.node.nodeDesc.pythonExecutable} {_MESHROOM_COMPUTE}"
+        meshroomComputeCmd += f" \"{chunk.node.graph.filepath}\" --node {chunk.node.name} --extern --inCurrentEnv"
 
         if len(chunk.node.getChunks()) > 1:
             meshroomComputeCmd += f" --iteration {chunk.range.iteration}"
